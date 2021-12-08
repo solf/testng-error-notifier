12f5be9004b3a2a776c2622e0a32ff0ac608eeff
Sergey Olefir <solf@users.noreply.github.com>
2021-12-08 09:20:40 -0400

    Code commit.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # testng-error-notifier
+
+Error reporter for TestNG.
+
+If you put this on Maven project test classpath:
+```
+		<dependency>
+			<groupId>io.github.solf</groupId>
+			<artifactId>testng-error-notifier</artifactId>
+			<version>${VERSION}</version>
+			<scope>test</scope>
+		</dependency>
+```		
+(replace version reference with any available versions from here:
+https://repo.maven.apache.org/maven2/io/github/solf/testng-error-notifier/ )
+
+then TestNG errors (exceptions) will be reported immediately to System.err
+and slf4j (if available) under 'testng-error-notifier' logger 
+along with the information about the test and timestamp -- this makes tracing
+logs much easier if you use multi-threaded tests execution.
+
+To easier find failure messages you can search for TESTNGFAIL
---
+++ src/main/java/io/github/solf/testngerrornotifier/TestngErrorNotificationListener.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright Sergey Olefir
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.solf.testngerrornotifier;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+
+import org.kohsuke.MetaInfServices;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestNGListener;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+/**
+ * Dumps error information right when it happens via System.err
+ *
+ * @author Sergey Olefir
+ */
+@MetaInfServices(ITestNGListener.class)
+public class TestngErrorNotificationListener extends TestListenerAdapter
+{
+
+	/* (non-Javadoc)
+	 * @see org.testng.TestListenerAdapter#onTestFailure(org.testng.ITestResult)
+	 */
+	@Override
+	public void onTestFailure(ITestResult tr)
+	{
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		final String errMsg = "TESTNGFAIL: Error in test: " + tr;
+		final String sysErrMsg = "[" + Thread.currentThread() + "] " + new Date() + " " + errMsg;
+		pw.print(sysErrMsg);
+		if (tr.getThrowable() != null)
+			tr.getThrowable().printStackTrace(pw);
+		else
+			pw.println("<no exception>");
+		
+		pw.flush();
+		System.err.println(sw.toString());
+		
+		try
+		{
+			LoggerFactory.getLogger("testng-error-notifier").error(errMsg, tr.getThrowable());
+		} catch (Throwable e)
+		{
+			// could be NoClassDefFoundError or w/e
+		}
+	}
+	
+}
